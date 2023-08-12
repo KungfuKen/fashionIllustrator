b4e5936491bb44bd00426fad19453599aac56f8e
KungfuKen <tenthani2@gmail.com>
2023-08-12 11:15:22 +0200

    text-color

--- pages/index.js
+++ pages/index.js
@@ -75,7 +75,7 @@ export default function Home() {
             type="text"
             name="prompt"
             placeholder="Enter a description here"
-            className="w-full font-mono bg-black px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-full font-mono bg-black text-white px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           <button
             type="submit"
