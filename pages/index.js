4fe594dae6b2bfa767c7a34e09bf09b31e89f010
KungfuKen <tenthani2@gmail.com>
2023-08-29 14:09:00 +0200

    pricing wording

--- pages/index.js
+++ pages/index.js
@@ -154,8 +154,8 @@ export default function Home() {
             Pricing 
           </h1>
           <p className="font-bold text-white font-lg">
-            The pricing is yet to be announced but will be between $15 and $25 per month and free
-            trials will be offered.
+            The pricing is yet to be confirmed but free trials will be offered and pricing will be around 
+            $15 to $25 per month. 
           </p>
           <div className="rounded-2xl p-8">
             <Image
@@ -268,8 +268,8 @@ export default function Home() {
                 Pricing 
               </h1>
               <p className="text-2xl text-white font-lg">
-                The pricing is yet to be announced but will be between $15 and $25 per month and free
-                trials will be offered. 
+                The pricing is yet to be confirmed but free trials will be offered and pricing will be around 
+                $15 to $25 per month. 
               </p>
             </div>
             <div className="rounded-2xl p-8 w-1/2">
